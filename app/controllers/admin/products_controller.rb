fad0df322e3673f22cfd25da8aca73c31472ae69
Zhao Wang <wangz2266@gmail.com>
2020-04-01 10:37:05 -0700

    admin authentication and restore the example files

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
 class Admin::ProductsController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'],
+                               password: ENV['ADMIN_PASSWORD']
 
   def index
     @products = Product.order(id: :desc).all
@@ -12,7 +16,7 @@ class Admin::ProductsController < ApplicationController
     @product = Product.new(product_params)
 
     if @product.save
-      redirect_to [:admin, :products], notice: 'Product created!'
+      redirect_to %i[admin products], notice: 'Product created!'
     else
       render :new
     end
@@ -21,7 +25,7 @@ class Admin::ProductsController < ApplicationController
   def destroy
     @product = Product.find params[:id]
     @product.destroy
-    redirect_to [:admin, :products], notice: 'Product deleted!'
+    redirect_to %i[admin products], notice: 'Product deleted!'
   end
 
   private
@@ -36,5 +40,4 @@ class Admin::ProductsController < ApplicationController
       :price
     )
   end
-
 end
